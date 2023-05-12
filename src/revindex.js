27732fced75b09573766c312f68514edfdbd65bf
JulitaDworak <julita.dworak@gmail.com>
2023-05-12 17:42:23 +0200

    hw11

--- src/revindex.js
+++ src/revindex.js
@@ -1,25 +1,23 @@
 import './css/styles.css';
-import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 import axios from 'axios';
 
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-
 const searchFormEl = document.getElementById('searchInput');
 const searchBtn = document.getElementById('searchBtn');
 const loadMoreBtn = document.getElementById('load-more');
 const galleryEl = document.querySelector('.gallery');
-const DEBOUNCE_DELAY = 2000;
+const perPage = 5;
+let page = 1;
+let totalPages = 0
+let query =''
+let newImages =''
+
 
 const BASE_API_URL = 'https://pixabay.com/api';
 const KEY = '35951390-f6c6ef4470c78e55c6c9cf8e4';
-let page = 1;
-const perPage = 5;
-let totalPages = 0
-let query =''
-
 const queryParams = new URLSearchParams({
   image_type: 'photo',
   orientation: 'horizontal',
@@ -28,19 +26,18 @@ const queryParams = new URLSearchParams({
   per_page: perPage,
 });
 
-loadMoreBtn.style.visibility = "hidden"
 
+
+// funkcja, która pobiera dane  z serwera
 async function fetchImages() {
   try {
     query = searchFormEl.value;
     const response = await axios.get(
       `${BASE_API_URL}?key=${KEY}&q=${query}&${queryParams}`
     );
-    if (response.data.hits.length === 0) {
-     throw new Error()}
+    if (response.data.hits.length === 0) 
+     throw new Error();
      return response.data
-    
-
   } catch (error) {
     Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
@@ -48,9 +45,12 @@ async function fetchImages() {
   }
 }
 
+
+
+// funkcja która wyswietla dane na stronie
 async function renderGallery() {
   const newImages = await fetchImages();
-  const imagesHTML = newImages.hits
+  const imagesHTML = await newImages.hits
     .map(image => ` <div class="photo-card">
     <a href="${image.largeImageURL}">
   <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
@@ -74,51 +74,39 @@ async function renderGallery() {
  galleryEl.innerHTML = imagesHTML;
  new SimpleLightbox('.gallery a', { });
 }
-    
-// async function createPage() {
-     
-//   await renderGallery();
-//     galleryEl.innerHTML=  ""
-//     const newImages = await fetchImages();
-//     Notiflix.Notify.success(`Hooray! We found ${newImages.totalHits} images.`);
-//     loadMoreBtn.style.visibility = "visible"
-//   }
-  async function createPage() {
+
+// funkcja która utworzy nową stronę 
+async function createPage() {
     const newImages = await fetchImages();
     Notiflix.Notify.success(`Hooray! We found ${newImages.totalHits} images.`);
-    totalPages = newImages.totalHits/ perPage
-    galleryEl.innerHTML=  ""
-    loadMoreBtn.style.visibility = "visible"
+    totalPages = newImages.totalHits / perPage
     newImages.hits.forEach(image => renderGallery(image))
-    // console.log(totalPages);
-    // console.log(newImages.totalHits);
-  }
-  
-function createNewGallery (e) {
+
+}
+
+// funkcja, która umożliwa załadowanie kolejnych stron
+async function nextPage () {
+    page ++;
+    const newImages = await fetchImages()
+  try{
+        if(page > totalPages) {
+        throw new Error(error)}
+        newImages.hits.forEach(image => renderGallery(image))
+        } catch {
+        Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.")}
+    }
+
+
+async function createNewGallery (e) {
   e.preventDefault()
   query = searchFormEl.value;
   page = 1;
   totalPages = 0;
   galleryEl.innerHTML = '';
   loadMoreBtn.style.visibility = "visible";
-  createPage()
-
+  await createPage()
 }
 
 searchBtn.addEventListener('click', createNewGallery)
 
-
-async function nextPage () {
-  page ++;
-  const newImages = await fetchImages();
-  newImages.hits.forEach(image => renderGallery(image))
-  if (page > newImages.totalHits/ perPage + 1 ) {
-      Notiflix.Notify.info(
-          "We're sorry, but you've reached the end of search results."
-        );
-  }
-};
-
-loadMoreBtn.addEventListener('click' , async () => {
-  await nextPage()
-})
+loadMoreBtn.addEventListener('click', ()=> nextPage())
